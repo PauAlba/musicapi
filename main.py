ac45c89e1a99d617930be615bee0b2f3fda89c0d
Paulina <paulinaorozcolsb@gmail.com>
2025-12-10 01:09:05 -0600

    fix:implemente manejo de ssl en pymysql/Aiven

--- main.py
+++ main.py
@@ -30,8 +30,17 @@ if not DATABASE_URL:
     # DATABASE_URL = ""
     pass
 
+CONNECT_ARGS = {
+    "ssl": {
+        "ssl_mode": "preferred" 
+    }
+}
 # Crear conexión
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL, 
+    pool_pre_ping=True,
+    connect_args=CONNECT_ARGS 
+)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
